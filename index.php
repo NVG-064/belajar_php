9e30351270edbf3c235086801459584f0ed36238
NVG-064 <aryslow92@gmail.com>
2024-01-24 13:17:00 +0700

    Initial project

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+
+<body>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Aksi</th>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td><img src="" width="60"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <a href="">ubah</a> | <a href="">hapus</a>
+        </tr>
+    </table>
+
+</body>
+
+</html>
